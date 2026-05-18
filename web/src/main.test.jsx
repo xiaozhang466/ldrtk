5f3e011b1d1426812d1074ed2c640c02d676e723
Tasy-code <1435071881@qq.com>
2026-05-18 20:03:13 +0800

    518,修复bug5

--- web/src/main.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TestPage from './App.test.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TestPage />
-  </React.StrictMode>
-)
